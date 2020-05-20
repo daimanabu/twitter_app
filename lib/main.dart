83a27a4c6605bfcd47dd8d04ae714721fc4bdd14
daimanabu <daimanabu777@gmail.com>
2020-05-20 16:30:16 +0900

    ヘッダー作成

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'header.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.pink[100],
+      ),
+      home: Scaffold(
+        appBar: Header(),
+        body: Center(child: Text('ホーム')),
+      ),
+    );
+  }
+}
